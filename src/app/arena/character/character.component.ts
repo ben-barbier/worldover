b60276284a14091028bbde95645c1bc3df59fb4b
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-08-06 09:53:43 +0200

    :lipstick: display character body instead of head in square

--- src/app/arena/character/character.component.ts
+++ src/app/arena/character/character.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {Character} from '../../store/models/character.model';
 import {AppState} from '../../store/app.state';
 import {Store} from '@ngrx/store';
@@ -11,13 +11,39 @@ import {selectCharacter} from '../../store/actions/selected-character.actions';
 })
 export class CharacterComponent {
 
+    private static readonly charactersByRow = 12;
+    private static readonly charactersByColumn = 8;
+
     @Input()
     public character: Character;
 
     constructor(private store: Store<AppState>) {
     }
 
+    @ViewChild('canvasElement', {static: false})
+    set canvas(canvasElement: ElementRef) {
+        const canvas: HTMLCanvasElement = canvasElement.nativeElement;
+        const image = new Image();
+        image.src = 'assets/characters/bodies/characters-bodies.png';
+        const ctx = canvas.getContext('2d');
+        image.onload = () => {
+            const characterWidth = image.width / CharacterComponent.charactersByRow;
+            const characterHeight = image.height / CharacterComponent.charactersByColumn;
+            canvas.setAttribute('width', `${characterWidth}px`);
+            canvas.setAttribute('height', `${characterHeight}px`);
+            ctx.drawImage(image,
+                characterWidth + ((this.character.photo % 4) - 1) * 3 * characterWidth, // Start X (px)
+                Math.floor(this.character.photo / 4) * 4 * characterHeight,   // Start Y (px)
+                characterWidth, // Width (px)
+                characterHeight, // Height (px)
+                0, 0,   // Place the result at 0, 0 in the canvas,
+                characterWidth, characterHeight // With as width / height: 32 * 32 (scale)
+            );
+        };
+    }
+
     public selectCharacter(character: Character) {
         this.store.dispatch(selectCharacter({character}));
     }
+
 }
